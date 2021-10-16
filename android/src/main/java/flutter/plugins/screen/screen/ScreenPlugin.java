8f875e4862e03ebf0763a0c653c34b846a6f8433
AttalliAyoub <attalliayoub50@gmail.com>
2021-10-17 00:11:37 +0100

    add engine v2 to screen

--- android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
+++ android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
@@ -1,27 +1,50 @@
 package flutter.plugins.screen.screen;
 
+import android.app.Activity;
 import android.provider.Settings;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 
 /**
  * ScreenPlugin
  */
-public class ScreenPlugin implements MethodCallHandler {
+// MethodCallHandler
+public class ScreenPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
-  private ScreenPlugin(Registrar registrar){
-    this._registrar = registrar;
+  private static String CHANNEL = "github.com/clovisnicolas/flutter_screen";
+  private MethodChannel channel;
+  private Activity activity;
+
+  //private ScreenPlugin(Registrar registrar){
+    //this._registrar = registrar;
+  // }
+  // private Registrar _registrar;
+
+  // public static void registerWith(Registrar registrar) {
+  //  final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL);
+  //  channel.setMethodCallHandler(new ScreenPlugin(registrar));
+  //}
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL);
+    binding.getApplicationContext().getContentResolver();
+    channel.setMethodCallHandler(this);
   }
-  private Registrar _registrar;
 
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/clovisnicolas/flutter_screen");
-    channel.setMethodCallHandler(new ScreenPlugin(registrar));
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
 
   @Override
@@ -32,24 +55,24 @@ public class ScreenPlugin implements MethodCallHandler {
         break;
       case "setBrightness":
         double brightness = call.argument("brightness");
-        WindowManager.LayoutParams layoutParams = _registrar.activity().getWindow().getAttributes();
+        WindowManager.LayoutParams layoutParams = activity.getWindow().getAttributes();
         layoutParams.screenBrightness = (float)brightness;
-        _registrar.activity().getWindow().setAttributes(layoutParams);
+        activity.getWindow().setAttributes(layoutParams);
         result.success(null);
         break;
       case "isKeptOn":
-        int flags = _registrar.activity().getWindow().getAttributes().flags;
+        int flags = activity.getWindow().getAttributes().flags;
         result.success((flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0) ;
         break;
       case "keepOn":
         Boolean on = call.argument("on");
         if (on) {
           System.out.println("Keeping screen on ");
-          _registrar.activity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+          activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
         else{
           System.out.println("Not keeping screen on");
-          _registrar.activity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+          activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
         result.success(null);
         break;
@@ -61,10 +84,10 @@ public class ScreenPlugin implements MethodCallHandler {
   }
 
   private float getBrightness(){
-    float result = _registrar.activity().getWindow().getAttributes().screenBrightness;
+    float result = activity.getWindow().getAttributes().screenBrightness;
     if (result < 0) { // the application is using the system brightness
       try {
-        result = Settings.System.getInt(_registrar.context().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
+        result = Settings.System.getInt(activity.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
       } catch (Settings.SettingNotFoundException e) {
         result = 1.0f;
         e.printStackTrace();
@@ -73,4 +96,23 @@ public class ScreenPlugin implements MethodCallHandler {
     return result;
   }
 
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    activity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    activity = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+    activity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    activity = null;
+  }
 }
